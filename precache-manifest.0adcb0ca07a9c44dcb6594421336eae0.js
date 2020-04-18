28664ac4f4bc91f14d8222229425d6ade22c4895
sanday08 <sandip.job89@gmail.com>
2020-04-18 23:07:46 +0530

    Updates

---
+++ precache-manifest.0adcb0ca07a9c44dcb6594421336eae0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a157addb06e63b79afb257dfac0639",
+    "url": "/contact_list/index.html"
+  },
+  {
+    "revision": "2129125f05fc04c6ba2b",
+    "url": "/contact_list/static/js/2.fd889d05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact_list/static/js/2.fd889d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1051515bbb4ba8d5cb5c",
+    "url": "/contact_list/static/js/main.ffdfd0dc.chunk.js"
+  },
+  {
+    "revision": "66f5e03e44059857ac02",
+    "url": "/contact_list/static/js/runtime-main.e193c36b.js"
+  }
+]);
